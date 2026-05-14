eec8780942b5014ad4f473322c34198f4dc0c54a
isabel-veloso <169570440+isabel-veloso@users.noreply.github.com>
2026-05-15 01:00:42 +0800

    Remove config.json from authorize middleware

--- _middleware/authorize.ts
+++ _middleware/authorize.ts
@@ -1,8 +1,7 @@
 import jwt from 'express-jwt';
-import config from '../config.json';
 import db from '../_helpers/db';
 
-const { secret } = config;
+const secret = process.env.JWT_SECRET || 'veloso-jwt-secret-ipt2026';
 
 export default function authorize(roles: any = []) {
     if (typeof roles === 'string') {
