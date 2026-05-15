f9b65bd195eed999a276a36a963059ae10f35082
isabel-veloso <169570440+isabel-veloso@users.noreply.github.com>
2026-05-15 11:58:18 +0800

    Fix SMTP port to 465 for Render

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -3,7 +3,8 @@ import nodemailer from 'nodemailer';
 export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.GMAIL_USER,
             pass: process.env.GMAIL_APP_PASSWORD,
