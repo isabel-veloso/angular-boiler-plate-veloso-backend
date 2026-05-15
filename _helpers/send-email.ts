64dbed23d5eb139de1dc9c76ba27b1ec09ce284d
isabel-veloso <169570440+isabel-veloso@users.noreply.github.com>
2026-05-15 13:45:19 +0800

    Fix SMTP with TLS config

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -3,12 +3,15 @@ import nodemailer from 'nodemailer';
 export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.GMAIL_USER,
             pass: process.env.GMAIL_APP_PASSWORD,
         },
+        tls: {
+            rejectUnauthorized: false
+        }
     });
     await transporter.sendMail({ from, to, subject, html });
 }
